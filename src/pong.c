faf4d71fd0c20d35553fdecae4ab875422bcbd91
Guilherme Vicentin <gvicentin@ciandt.com>
2023-02-21 14:27:20 -0300

    New ball reflection when hitting the paddles

--- src/pong.c
+++ src/pong.c
@@ -13,28 +13,32 @@
 #define COLOR_BG      BLACK
 #define COLOR_ENITIES WHITE
 
-#define PADDLE_WIDTH  30
-#define PADDLE_HEIGHT 100
-#define PADDLE_SPEED  600
+#define PADDLE_WIDTH      20
+#define PADDLE_HEIGHT     80
+#define PADDLE_SPEED      600
+#define PADDLE_HOR_OFFSET 30
 
-#define BALL_WIDTH         20
-#define BALL_HEIGHT        20
+#define BALL_WIDTH         15
+#define BALL_HEIGHT        15
 #define BALL_INITIAL_SPEED 300
 
+#define BORDER_WIDTH 20
+
 typedef struct Entity {
-    Rectangle rect;
-    Vector2 dir;
-    float speed;
+    Rectangle rect; // position and dimensions
+    Vector2 dir;    // normilized direction
+    float speed;    // velocity multiplier
 } Entity;
 
 typedef struct CollisionData {
-    bool hit;
-    float time;
-    Vector2 contactPoint;
-    Vector2 contactNormal;
+    bool hit;              // true if collision has happened
+    float time;            // time for collision [0.0,1.0]
+    Vector2 contactPoint;  // collision point for restitution
+    Vector2 contactNormal; // surface normal where collide
 } CollisionData;
 
 static Entity leftPaddle, rightPaddle, ball;
+static int topLimit, rightLimit, bottomLimit, leftLimit;
 
 // -----------------------------------------------------------------------------
 // Module declaration
@@ -58,7 +62,7 @@ static CollisionData SweptAABB(Rectangle rect, Vector2 vel, Rectangle target);
 // -----------------------------------------------------------------------------
 int main(void) {
     // pre configuration
-    SetTraceLogLevel(LOG_ERROR);
+    SetTraceLogLevel(LOG_DEBUG);
 
     // initialization
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE);
@@ -97,10 +101,17 @@ void ResetGame(void) {
                     .dir = (Vector2){0},
                     .speed = BALL_INITIAL_SPEED};
 
+    // limits
+    topLimit = BORDER_WIDTH;
+    rightLimit = SCREEN_WIDTH - PADDLE_HOR_OFFSET;
+    bottomLimit = SCREEN_HEIGHT - BORDER_WIDTH;
+    leftLimit = PADDLE_HOR_OFFSET;
+
     // reset paddle positions
-    leftPaddle.rect.x = 0;
+    leftPaddle.rect.x = PADDLE_HOR_OFFSET;
     leftPaddle.rect.y = (SCREEN_HEIGHT - leftPaddle.rect.height) / 2.0f;
-    rightPaddle.rect.x = SCREEN_WIDTH - rightPaddle.rect.width;
+    rightPaddle.rect.x =
+        SCREEN_WIDTH - PADDLE_HOR_OFFSET - rightPaddle.rect.width;
     rightPaddle.rect.y = leftPaddle.rect.y;
 
     ResetBall();
@@ -109,13 +120,9 @@ void ResetGame(void) {
 static void ResetBall(void) {
     ball.rect.x = (SCREEN_WIDTH - ball.rect.width) / 2.0f;
     ball.rect.y = (SCREEN_HEIGHT - ball.rect.height) / 2.0f;
-    ball.dir.x = (float)GetRandomValue(0, 1);
-    ball.dir.y = (float)GetRandomValue(0, 1);
-
-    do {
-        // don't let the ball stuct
-        ball.dir = Vector2Normalize(ball.dir);
-    } while (Vector2Equals(ball.dir, Vector2Zero()));
+    ball.dir.x = GetRandomValue(0, 1) == 0 ? -1.0f : 1.0f;
+    ball.dir.y = GetRandomValue(0, 1000) / 1000.0f;
+    ball.dir = Vector2Normalize(ball.dir);
 }
 
 static void GameLoop(void) {
@@ -126,24 +133,83 @@ static void GameLoop(void) {
     // update paddles
     float dt = GetFrameTime();
     rightPaddle.rect.y += rightPaddle.dir.y * rightPaddle.speed * dt;
-    leftPaddle.dir.y = (ball.rect.y - leftPaddle.rect.y > 0.0f) ? 1.0f : -1.0f;
-    leftPaddle.rect.y += leftPaddle.dir.y * leftPaddle.speed * dt;
+    leftPaddle.rect.y = ball.rect.y;
+
+    // keep paddles on screen
+    if (rightPaddle.rect.y < topLimit) {
+        rightPaddle.rect.y = topLimit;
+    } else if (rightPaddle.rect.y + rightPaddle.rect.height > bottomLimit) {
+        rightPaddle.rect.y = bottomLimit - rightPaddle.rect.height;
+    }
+    if (leftPaddle.rect.y < topLimit) {
+        leftPaddle.rect.y = topLimit;
+    } else if (leftPaddle.rect.y + leftPaddle.rect.height > bottomLimit) {
+        leftPaddle.rect.y = bottomLimit - leftPaddle.rect.height;
+    }
 
     // update ball
     Vector2 ballVel = Vector2Scale(ball.dir, ball.speed * dt);
-    ball.rect.x += ballVel.x;
-    ball.rect.y += ballVel.y;
+    Rectangle ballRectSwept = SweptRectangle(ball.rect, ballVel);
+    CollisionData ballCollLeftPaddle = {0};
+    CollisionData ballCollRightPaddle = {0};
 
-    // keep paddles on screen
-    if (rightPaddle.rect.y < 0.0f) {
-        rightPaddle.rect.y = 0.0f;
-    } else if (rightPaddle.rect.y + rightPaddle.rect.height > SCREEN_HEIGHT) {
-        rightPaddle.rect.y = SCREEN_HEIGHT - rightPaddle.rect.height;
+    if (AABBCheck(ballRectSwept, leftPaddle.rect)) {
+        ballCollLeftPaddle = SweptAABB(ball.rect, ballVel, leftPaddle.rect);
+        if (ballCollLeftPaddle.hit) {
+            ball.rect.x = ballCollLeftPaddle.contactPoint.x;
+            ball.rect.y = ballCollLeftPaddle.contactPoint.y;
+
+            if (ballCollLeftPaddle.contactNormal.x == 0) {
+                // colided from top or bottom
+                ball.dir.y *= -1.0f;
+            } else {
+                // collided from the front
+                ball.dir.x *= -1.0f;
+                ball.dir.y =
+                    2 * (ball.rect.y - leftPaddle.rect.y + ball.rect.height) /
+                        (PADDLE_HEIGHT + ball.rect.height) -
+                    1.0f;
+                ball.dir = Vector2Normalize(ball.dir);
+            }
+        }
     }
-    if (leftPaddle.rect.y < 0.0f) {
-        leftPaddle.rect.y = 0.0f;
-    } else if (leftPaddle.rect.y + leftPaddle.rect.height > SCREEN_HEIGHT) {
-        leftPaddle.rect.y = SCREEN_HEIGHT - leftPaddle.rect.height;
+    if (AABBCheck(ballRectSwept, rightPaddle.rect)) {
+        ballCollRightPaddle = SweptAABB(ball.rect, ballVel, rightPaddle.rect);
+        if (ballCollRightPaddle.hit) {
+            // resolve and reflect
+            ball.rect.x = ballCollRightPaddle.contactPoint.x;
+            ball.rect.y = ballCollRightPaddle.contactPoint.y;
+            if (ballCollRightPaddle.contactNormal.x == 0) {
+                // colided from top or bottom
+                ball.dir.y *= -1.0f;
+            } else {
+                // collided from the front
+                ball.dir.x *= -1.0f;
+                ball.dir.y =
+                    2 * (ball.rect.y - rightPaddle.rect.y + ball.rect.height) /
+                        (PADDLE_HEIGHT + ball.rect.height) -
+                    1.0f;
+                ball.dir = Vector2Normalize(ball.dir);
+            }
+        }
+    }
+
+    if (!ballCollLeftPaddle.hit && !ballCollRightPaddle.hit) {
+        ball.rect.x += ballVel.x;
+        ball.rect.y += ballVel.y;
+    }
+
+    // reflect ball screen border
+    if (ball.rect.y < topLimit) {
+        ball.rect.y = topLimit;
+        ball.dir.y *= -1.0f;
+    } else if (ball.rect.y + ball.rect.height > bottomLimit) {
+        ball.rect.y = bottomLimit - ball.rect.height;
+        ball.dir.y *= -1.0f;
+    }
+
+    if (ball.rect.x + ball.rect.width < 0.0f || ball.rect.x > SCREEN_WIDTH) {
+        ResetBall();
     }
 
     DrawGame();
@@ -165,6 +231,11 @@ static float KeyboardInput(void) {
 static void DrawGame(void) {
     BeginDrawing();
     ClearBackground(BLACK);
+
+    // draw borders
+    DrawRectangle(0, 0, SCREEN_WIDTH, BORDER_WIDTH, COLOR_ENITIES);
+    DrawRectangle(0, SCREEN_HEIGHT - BORDER_WIDTH, SCREEN_WIDTH, BORDER_WIDTH,
+                  COLOR_ENITIES);
 
     DrawRectangleRec(leftPaddle.rect, WHITE);
     DrawRectangleRec(rightPaddle.rect, WHITE);
